77419e782b168bbca8e37c82ef6ba30746afba6a
matheus anmo <matheusanmo@gmail.com>
2021-04-22 11:26:09 -0300

    expande docs IOHandler. implementa print_help.

--- source/main.cpp
+++ source/main.cpp
@@ -5,18 +5,39 @@ using std::string;
 #include <istream>
 #include <ostream>
 
+/**
+ * term
+ */
 struct Term {
     size_type m_weight;
     string    m_query;
     Term(size_type weight, const string& query);
 }; // struct Term
 
+/**
+ * iohandler
+ */
 class IOHandler {
+    ////////////////////
+    // Membros privados
+    ////////////////////
     private:
         std::istream m_istream;
         std::ostream m_ostream;
         string       m_database_filename;
 
+    //////////
+    // Testes
+    //////////
+    private:
+        bool test_constructor(void);
+        bool test_request_line(void);
+        bool test_present_terms(void);
+        bool test_readlines(void);
+
+    ////////////////////
+    // Metodos publicos
+    ////////////////////
     public:
         IOHandler(std::istream is, std::ostream os, string dbfilename);
         string request_line(void);
@@ -24,6 +45,9 @@ class IOHandler {
         std::array<string> read_lines(void);
 }; //class IOHandler
 
+/** 
+ * dbhandler
+ */
 class DBHandler {
     private:
         std::array<Term> m_database;
@@ -33,10 +57,28 @@ class DBHandler {
         std::array<const Term&> get_terms_unstable_refs(string prefix);
 }; // class DBHandler
 
+/**
+ * print help
+ */
 void print_help() {
+    using std::cout;
+    using std::endl;
+    cout << "% ./autocomplete\n";
+    cout << "Usage: autocomplete <database_file>\n";
+    cout << "  Where <database_file> is the ascii file that contains the query terms and weights." << endl;
     return;
 }
 
+/**
+ * test_routine
+ */
+void test_routine() {
+    return;
+}
+
+/** 
+ * main
+ */
 int main(int argc, char** argv) {
     if (argc < 2) {
         print_help();
